3d0d1bc5d6ef544c03fb0221f4dc6197d8589ef1
Sreenath732 <sreenath.r@finecho.net>
2022-08-01 15:39:31 +0530

    Adds CircleCI config file

--- main-test.py
+++ main-test.py
@@ -5,7 +5,7 @@ def TestAdd():
         assert Add(2,3) == 5
 	assert Add(3,3) == 6
 	assert Add(5,5) == 10
-	assert Add(7,5) == 10
+	assert Add(7,5) == 12
         print("Add Function works correctly")
 
 if __name__ == '__main__':
